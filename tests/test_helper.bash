5ad27279745362c7f3f328794333b1fb46589e6e
Juan Ibiapina <juanibiapina@gmail.com>
2014-06-24 19:53:30 -0300

    Rename directories for testing.

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -10,7 +10,10 @@ export PATH="${BASHER_TEST_DIR}/bin:$PATH"
 
 mkdir -p "${BASHER_ROOT}/libexec"
 mkdir -p "${BASHER_ROOT}/tests"
-mkdir -p "${BASHER_ROOT}/modules"
+mkdir -p "${BASHER_ROOT}/cellar"
+mkdir -p "${BASHER_ROOT}/cellar/bin"
+mkdir -p "${BASHER_ROOT}/cellar/modules"
+
 mkdir -p "${BASHER_TEST_DIR}/bin"
 
 teardown() {
