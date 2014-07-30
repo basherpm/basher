d020903a4cebd12199c9709578381ed16d707bb2
Juan Ibiapina <juanibiapina@gmail.com>
2014-07-30 10:51:51 -0300

    Fix sed inplace substitution.
    The -i option is not portable.
    
    http://stackoverflow.com/questions/10811458/how-to-fix-sed-inplace-replacement-mac-os-and-ubuntu-issue

--- tests/lib/package_helpers.bash
+++ tests/lib/package_helpers.bash
@@ -28,7 +28,8 @@ create_exec() {
 
   if [ -e "package.sh" ]; then
     if grep -sq "BIN=" "package.sh"; then
-      sed -e "/^BIN=/ s/$/:bin\/$exec/" -i'' package.sh
+      sed -e "/^BIN=/ s/$/:bin\/$exec/" package.sh > package.sh.tmp
+      mv package.sh.tmp package.sh
     else
       echo "BIN=bin/$exec" >> package.sh
     fi
