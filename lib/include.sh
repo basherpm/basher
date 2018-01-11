3fe36c6ac5f95b5019c8166ef3d7e469942cc72f
Hugo Thunnissen <hugo.thunnissen@gmail.com>
2018-01-11 01:00:55 +0100

    Redirect error messages to stderr and make variable scope local.
    - By redirecting the error messages to stderr, it will be clear for the user what went wrong even when
      piping stdout through another program or capturing its output in a subshell.
    - By making the variable scope local the shell's global state remains untouched.

--- lib/include.sh
+++ lib/include.sh
@@ -1,21 +1,21 @@
 include() {
-  package="$1"
-  file="$2"
+  declare package="$1"
+  declare file="$2"
 
   if [ -z "$package" ] || [ -z "$file" ]; then
-    echo "Usage: include <package> <file>"
+    echo "Usage: include <package> <file>" >&2
     return 1
   fi
 
   if [ ! -e "$BASHER_PREFIX/packages/$package" ]; then
-    echo "Package not installed: $package"
+    echo "Package not installed: $package" >&2
     return 1
   fi
 
   if [ -e "$BASHER_PREFIX/packages/$package/$file" ]; then
-    source "$BASHER_PREFIX/packages/$package/$file"
+    source "$BASHER_PREFIX/packages/$package/$file" >&2
   else
-    echo "File not found: $BASHER_PREFIX/packages/$package/$file"
+    echo "File not found: $BASHER_PREFIX/packages/$package/$file" >&2
     return 1
   fi
 }
